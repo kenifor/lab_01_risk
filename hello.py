b41b7e88447d208c48b4defb37f0a716f5312b10
Denis Balashov <balasovdenis74@gmail.com>
2025-11-26 15:44:43 +0300

    hello.py update

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-import sys,base64 as b,os
+import sys,base64 as b
 p=lambda x:sys.stdout.write(x)
-p(b.b64decode(b'SGVsbG8gYXBwc2Vjd29ybGQ=\n').decode())
+p(b.b64decode(b'SGVsbG8gYXBwc2Vjd29ybGQ=').decode()+f' from {sys.stdin.readline().strip()}\n')
